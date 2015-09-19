7f6b1704803c812d8987dfc547151da7d2e7139e
George Pal <georgepal@gmail.com>
2015-09-19 07:29:00 +0200

    add ROLES for users

--- laravel/database/seeds/RoleTableSeeder.php
+++ laravel/database/seeds/RoleTableSeeder.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use App\Role;
 
-class RolesTableSeeder extends Seeder{
+class RoleTableSeeder extends Seeder{
 
     public function run()
     {
